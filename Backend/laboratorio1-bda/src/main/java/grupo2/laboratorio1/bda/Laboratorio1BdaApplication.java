9e374fb95410213dac25afd94933094ec3a25c3d
diego oliva <diego.oliva@usach.cl>
2023-04-18 20:49:22 -0400

    [Diego Oliva] - Inicialización de proyecto
    Creación de Frontend y Backend

---
+++ Backend/laboratorio1-bda/src/main/java/grupo2/laboratorio1/bda/Laboratorio1BdaApplication.java
@@ -0,0 +1,13 @@
+package grupo2.laboratorio1.bda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio1BdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio1BdaApplication.class, args);
+	}
+
+}
---
+++ Backend/laboratorio1-bda/src/test/java/grupo2/laboratorio1/bda/Laboratorio1BdaApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.laboratorio1.bda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio1BdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
