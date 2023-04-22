3a6ac25f8a6cbf9b52affbaafcb54e03a80b5356
cristowo <cristobal.marchant@usach.cl>
2023-04-21 23:53:59 -0400

    [Cristóbal Marchant] Actualización conexión database

--- Backend/laboratorio1-bda/src/main/java/grupo2/laboratorio1/bda/repositories/DatabaseContext.java
+++ Backend/laboratorio1-bda/src/main/java/grupo2/laboratorio1/bda/repositories/DatabaseContext.java
@@ -1,17 +1,23 @@
 package grupo2.laboratorio1.bda.repositories;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sql2o.Sql2o;
 
 @Configuration
 public class DatabaseContext {
-    private String URL = "jdbc:postgresql://localhost:5432/desastresdb";
-    private String USER = "postgres";
-    private String PASSWORD;
+    @Value("${database.url}")
+    private String dbUrl;
+
+    @Value("${database.user}")
+    private String dbUser;
+
+    @Value("${database.password}")
+    private String dbPass;
 
     @Bean
     public Sql2o sql2o(){
-        return new Sql2o(URL, USER, PASSWORD);
+        return new Sql2o(dbUrl, dbUser, dbPass);
     }
 }
