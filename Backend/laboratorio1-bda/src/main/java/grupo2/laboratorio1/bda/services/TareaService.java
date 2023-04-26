a8b60132c124f1acda49c71adf5a52e06bcd44d2
diego oliva <diego.oliva@usach.cl>
2023-04-26 19:34:14 -0400

    [Diego Oliva] Pequeñas modificaciones

--- Backend/laboratorio1-bda/src/main/java/grupo2/laboratorio1/bda/services/TareaService.java
+++ Backend/laboratorio1-bda/src/main/java/grupo2/laboratorio1/bda/services/TareaService.java
@@ -1,7 +1,7 @@
 package grupo2.laboratorio1.bda.services;
 
 import grupo2.laboratorio1.bda.models.Tarea;
-import grupo2.laboratorio1.bda.repositories.TareaRepository;
+import grupo2.laboratorio1.bda.repositories.ITareaRepository;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,22 +12,18 @@ import java.util.List;
 @Service
 public class TareaService {
     @Autowired
-    TareaRepository tareaRepository;
+    ITareaRepository tareaRepository;
 
     public void createTarea(@NonNull Integer idEmergencia, String nombre, String descripcion, Integer cantVolRequeridos,
                             Integer cantVolInscritos, Date fechaInicio, Date fechaFin , String estadoActual)
     {Tarea tarea = new Tarea( null,idEmergencia, nombre, descripcion,cantVolRequeridos, cantVolInscritos, fechaInicio, fechaFin, estadoActual);
-        validarTarea(tarea);
         tareaRepository.createTarea(tarea);}
 
-    private void validarTarea(Tarea tarea){
-        
-    }
+    
 
     public Tarea getTarea(@NonNull Integer idTarea){
         Tarea tarea = new Tarea();
         tarea.setIdTarea(idTarea);
-        validarTarea(tarea);
         return tareaRepository.getTarea(idTarea);
     }
 
@@ -37,7 +33,6 @@ public class TareaService {
 
     public void updateTarea(Integer idTarea, Tarea tarea){
         tarea.setIdTarea(idTarea);
-        validarTarea(tarea);
         tareaRepository.updateTarea(tarea);
     }
 
